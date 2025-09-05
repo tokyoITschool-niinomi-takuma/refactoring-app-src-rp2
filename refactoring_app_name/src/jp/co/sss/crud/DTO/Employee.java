8537aabb9904cead82831136a5e21cf9d40b6f6e
新實拓磨 <t_niinomi@braingate.co.jp>
2025-09-05 17:46:07 +0900

    add:DTOファイルを作成しDepartment.java,Employee.javaクラスを追加。
    理由：DBの検索結果を格納するため。

---
+++ refactoring_app_name/src/jp/co/sss/crud/DTO/Employee.java
@@ -0,0 +1,53 @@
+package jp.co.sss.crud.DTO;
+
+import java.sql.Date;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private int gender;
+	private Date birthday;
+	private int deptId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+}
