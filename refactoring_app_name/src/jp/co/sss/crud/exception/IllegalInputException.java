022dee813774974637441aef2b5fe092608bead8
新實拓磨 <t_niinomi@braingate.co.jp>
2025-09-09 17:51:47 +0900

    add:IllegalInputException.javaとSystemErroeException.javaを追加
    狙い：例外処理用のクラスの追加
    
    add:IEmployeeService.javaとIEmployeeDAO.javaの追加
    
    狙い：それぞれのクラスのインターフェイス

---
+++ refactoring_app_name/src/jp/co/sss/crud/exception/IllegalInputException.java
@@ -0,0 +1,9 @@
+package jp.co.sss.crud.exception;
+
+public class IllegalInputException extends Exception {
+
+	public IllegalInputException(String errorMsg) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
---
+++ refactoring_app_name/src/jp/co/sss/crud/exception/SystemErrorException.java
@@ -0,0 +1,15 @@
+package jp.co.sss.crud.exception;
+
+import java.io.IOException;
+
+public class SystemErrorException extends Exception {
+
+	public SystemErrorException() {
+
+	}
+
+	public SystemErrorException(String msgSystemError, IOException e) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
