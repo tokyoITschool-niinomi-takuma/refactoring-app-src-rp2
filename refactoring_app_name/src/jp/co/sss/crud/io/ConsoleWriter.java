08743ca26e11187c44c4cdacd73149acc1627eed
新實拓磨 <t_niinomi@braingate.co.jp>
2025-09-09 17:33:16 +0900

    add:ConsoleWriter.javaを追加
    狙い：コンソールに出力されるメッセージをまとめるため。
    
    delete:Display.javaを削除
    
    狙い：ConsoleWriter.java追加に伴い、指定のあったConsoleWriter.javaに出力をまとめるため。
    
    add:IConsoleReader.javaを追加
    
    狙い：入力処理用のインターフェイス
    
    fix:EmployeeDAOの全件検索と社員名検索の処理を改修中

---
+++ refactoring_app_name/src/jp/co/sss/crud/io/ConsoleWriter.java
@@ -0,0 +1,35 @@
+package jp.co.sss.crud.io;
+
+import java.util.List;
+
+import jp.co.sss.crud.DTO.Employee;
+import jp.co.sss.crud.util.ConstantMsg;
+
+public class ConsoleWriter {
+	/**
+	 * メニュー表示用のメソッド
+	 */
+	public static void showMenu() {
+		System.out.println(ConstantMsg.TITLE);
+		System.out.println(ConstantMsg.FIND_ALL);
+		System.out.println(ConstantMsg.SEARCH_EMP_NAME);
+		System.out.println(ConstantMsg.SEARCH_DEPT_ID);
+		System.out.println(ConstantMsg.INSERT);
+		System.out.println(ConstantMsg.UPDATE);
+		System.out.println(ConstantMsg.DELETE);
+		System.out.println(ConstantMsg.END);
+		System.out.print(ConstantMsg.SELECT_MENU_NO);
+	}
+
+	/**
+	 * 検索結果のコンソール表示
+	 * @param employees
+	 */
+	public static void showSearchreSult(List<Employee> employees) {
+		System.out.println(ConstantMsg.SEARCH_RESULT);
+		//
+		for (Employee employee : employees) {
+			System.out.println(employee);
+		}
+	}
+}
